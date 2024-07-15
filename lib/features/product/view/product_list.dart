f5f33aace6e06b9cec03cbab742a42837b34ae02
harshdeep <harshsinghsaluja10@gmail.com>
2024-07-15 14:05:31 +0530

    update

--- lib/features/product/view/product_list.dart
+++ lib/features/product/view/product_list.dart
@@ -26,60 +26,59 @@ class _ProductListScreenState extends State<ProductListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        fetchProducts();
-      }),
-      body: isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : FutureBuilder<List<ProductModel>>(
-              future: fetchProducts(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                    itemCount: products.length,
-                    itemBuilder: (context, index) {
-                      ProductModel product = products[index];
-                      return Card(
-                          child: Row(
-                        children: [
-                          Image.network(
-                            product.image,
-                            height: 120,
-                            width: 120,
-                            fit: BoxFit.cover,
-                          ),
-                          SizedBox(width: 20),
-                          Expanded(
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(product.title),
-                                Text(product.category),
-                                Text(
-                                  product.description,
-                                  maxLines: 2,
-                                  overflow: TextOverflow.ellipsis,
-                                ),
-                                Text('price ' + product.price.toString()),
-                                Text('ratings ' +
-                                    product.rating.count.toString()),
-                                Text('stars ' + product.rating.rate.toString()),
-                              ],
+      body: RefreshIndicator(
+        onRefresh: () async {
+          products = [];
+          setState(() {});
+          await fetchProducts();
+        },
+        child: FutureBuilder<List<ProductModel>>(
+          future: fetchProducts(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  ProductModel product = products[index];
+                  return Card(
+                      child: Row(
+                    children: [
+                      Image.network(
+                        product.image,
+                        height: 120,
+                        width: 120,
+                        fit: BoxFit.cover,
+                      ),
+                      SizedBox(width: 20),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(product.title),
+                            Text(product.category),
+                            Text(
+                              product.description,
+                              maxLines: 2,
+                              overflow: TextOverflow.ellipsis,
                             ),
-                          ),
-                        ],
-                      ));
-                    },
-                  );
-                } else {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              },
-            ),
+                            Text('price ' + product.price.toString()),
+                            Text('ratings ' + product.rating.count.toString()),
+                            Text('stars ' + product.rating.rate.toString()),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ));
+                },
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ),
+      ),
     );
   }
 }
